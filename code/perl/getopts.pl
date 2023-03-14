9dc65e7ce9b5cf9e7b9d74f0b3d9b36136530448
Dave Tang <davetingpongtang@gmail.com>
2023-03-14 09:34:16 +0900

    Positional and optional

--- code/perl/getopts.pl
+++ code/perl/getopts.pl
@@ -4,40 +4,44 @@ use warnings;
 use strict;
 use Getopt::Long;
 
-my $fastq1  = "";
-my $fastq2  = "";
 my $thread  = 8;
 my $verbose = 0;
 my $help    = 0;
 
-GetOptions ("thread=i" => \$thread,   # numeric
-            "first=s"  => \$fastq1,   # string
-            "second=s" => \$fastq2,   # string
-            "verbose"  => \$verbose,  # flag
-            "help"     => \$help)     # flag
-|| die("Error in command line arguments\n");
+GetOptions(
+   "thread=i" => \$thread,   # numeric
+   "verbose"  => \$verbose,  # flag
+   "help"     => \$help      # flag
+) or die("Error in command line arguments\n");
 
-if ($fastq1 eq "" || $fastq2 eq "" || $help == 1){
+if ($help == 1){
    usage()
 }
 
-if ($verbose){
-   print join("\n", "First: $fastq1", "Second: $fastq2", "Thread: $thread"), "\n";
+if (scalar @ARGV != 2){
+   usage()
 }
+my $fastq1  = $ARGV[0];
+my $fastq2  = $ARGV[1];
+
+if ($verbose){
+   print STDERR "Verbose mode\n";
+}
+print STDERR join("\n", "Read 1: $fastq1", "Read 2: $fastq2", "Using $thread threads"), "\n";
 
 sub usage {
 print STDERR <<EOF;
-Usage: $0 -f read1.fastq -s read2.fastq -t 8
+Usage: $0 [-h] [-v] [-t INT] read1 read2
 
-Where:   -f, --first  FILE     first read pair
-         -s, --second FILE     second read pair
-         -t, --thread INT      number of threads to use (default 8)
-         -v, --verbose         print out arguments
-         -h, --help            this helpful usage message
+positional arguments:
+  read1                 first read pair
+  read2                 second read pair
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         verbose mode
+  -p INT --threads INT  number of threads (default: 8)
 
 EOF
 exit();
 }
-
-__END__
-
